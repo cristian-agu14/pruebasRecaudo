2c5f5914ff8a1952c799f4208f9a0a06e8cf69ea
windowsdev <cristian.agu14@gmail.com>
2018-02-05 10:21:58 -0500

    pruebas hasta convenio de recaudos full

--- MercuryTours/src/objectsEdeq/Convenio.java
+++ MercuryTours/src/objectsEdeq/Convenio.java
@@ -1,15 +1,126 @@
 package objectsEdeq;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 import base.PageBase;
 
-public class Convenio extends PageBase{
+public class Convenio extends PageBase {
 
 	public Convenio(WebDriver driver, String pageTitle) {
 		super(driver, pageTitle);
-		// TODO Auto-generated constructor stub
 	}
-	
 
+	public boolean crearConvenioRecaudo(String numero, String nombre, String posInicioCodEmpresa,
+			String posFinCodempresa, String posInicioReferencia, String posFinReferencia, String posInicioValor,
+			String posFinValor, String posInicioFecha, String posfinFecha, String ip, String puerto, String ruta,
+			String usuario, String contrasena, String cliente, String codigoEmpresa) {
+		
+		clickButtonLink(btnMenu);
+		clickButtonLink(opcionRecaudo);
+		clickButtonLink(pestanaConvenios);
+		clickButtonLink(tipoConvenioCliente);
+		//selectDropdownValue(listaClientes, cliente);
+		sendText(numeroConvenioCajaTexto, numero);
+		sendText(nombreConvenioCajaTexto, nombre);
+		sendText(posIncioCodEmpresaCajaTexto, posInicioCodEmpresa);
+		sendText(posFinCodEmpresaCajaTexto, posFinCodempresa);
+		sendText(posInicioReferenciaCajaTexto, posInicioReferencia);
+		sendText(posFinReferenciaCajaTexto, posFinReferencia);
+		sendText(posInicioValorCajaTexto, posInicioValor);
+		sendText(posFinValorCajaTexto, posFinValor);
+		sendText(posinicioFechaCajaTexto, posInicioFecha);
+		sendText(posFinalFechaCajaTexto, posfinFecha);
+		sendText(codigoEmpresaCajaTexto, codigoEmpresa);
+		sendText(ipCajaTexto, ip);
+		sendText(puertoCajaTexto, puerto);
+		sendText(rutaCajaTexto, ruta);
+		sendText(usuarioCajaTexto, usuario);
+		sendText(contrasenaCajaTexto, contrasena);
+		clickButtonLink(btnSubirLogoCajaTexto);
+		esperar(15);
+		
+		
+
+		return true;
+
+	}
+
+	@FindBy(how = How.XPATH, using = "//html/body/div[2]/div[2]/div[1]/div/img")
+	private WebElement btnMenu;
+
+	@FindBy(how = How.ID, using = "menu2")
+	private WebElement opcionRecaudo;
+
+	@FindBy(how = How.XPATH, using = "//html/body/div[2]/div[2]/div[2]/ul/li[4]/a")
+	private WebElement pestanaConvenios;
+
+	@FindBy(how = How.ID, using = "cliente")
+	private WebElement tipoConvenioCliente;
+	
+	@FindBy(how = How.ID, using= "clienteRecaudo")
+	private WebElement listaClientes;
+
+	@FindBy(how = How.ID, using = "proveedor")
+	private WebElement tipoConvenioProveedor;
+
+	@FindBy(how = How.ID, using = "ConvenioRecaudo_NUMERO_CONVENIO")
+	private WebElement numeroConvenioCajaTexto;
+
+	@FindBy(how = How.ID, using = "ConvenioRecaudo_NOMBRE")
+	private WebElement nombreConvenioCajaTexto;
+
+	@FindBy(how = How.ID, using = "formatoCodigoBarras_INICIAL_EMPRESA")
+	private WebElement posIncioCodEmpresaCajaTexto;
+
+	@FindBy(how = How.ID, using = "formatoCodigoBarras_FINAL_EMPRESA")
+	private WebElement posFinCodEmpresaCajaTexto;
+
+	@FindBy(how = How.ID, using = "formatoCodigoBarras_INICIAL_REFERENCIA")
+	private WebElement posInicioReferenciaCajaTexto;
+
+	@FindBy(how = How.ID, using = "formatoCodigoBarras_FINAL_REFERENCIA")
+	private WebElement posFinReferenciaCajaTexto;
+
+	@FindBy(how = How.ID, using = "formatoCodigoBarras_INICIAL_VALOR")
+	private WebElement posInicioValorCajaTexto;
+
+	@FindBy(how = How.ID, using = "formatoCodigoBarras_FINAL_VALOR")
+	private WebElement posFinValorCajaTexto;
+
+	@FindBy(how = How.ID, using = "formatoCodigoBarras_INICIAL_FECHA")
+	private WebElement posinicioFechaCajaTexto;
+
+	@FindBy(how = How.ID, using = "formatoCodigoBarras_FINAL_FECHA")
+	private WebElement posFinalFechaCajaTexto;
+
+	@FindBy(how = How.XPATH, using = "//html/body/div[2]/div[2]/div[2]/div[2]/fieldset/div/form/div[3]/div[2]/div[3]/div[1]/div/label/span")
+	private WebElement btnSubirLogoCajaTexto;
+
+	@FindBy(how = How.ID, using = "ConvenioRecaudo_NIT")
+	private WebElement codigoEmpresaCajaTexto;
+
+	@FindBy(how = How.ID, using = "ftpURL")
+	private WebElement ipCajaTexto;
+
+	@FindBy(how = How.ID, using = "ftpPUERTO")
+	private WebElement puertoCajaTexto;
+
+	@FindBy(how = How.ID, using = "ftpRUTAINTERNA")
+	private WebElement rutaCajaTexto;
+
+	@FindBy(how = How.ID, using = "ftpUSUARIO")
+	private WebElement usuarioCajaTexto;
+
+	@FindBy(how = How.ID, using = "ftpCONTRASENA")
+	private WebElement contrasenaCajaTexto;
+
+	/**
+	 * Parametro que representa el boton para terminar de guardar el cliente de
+	 * recaudo
+	 */
+	@FindBy(how = How.ID, using = "grabar")
+	private WebElement btnGrabarConvenioRecaudo;
 }
