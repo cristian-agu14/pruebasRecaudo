d1c77ada0eb2ad4df06e4545cff5425f8ac6b486
windowsdev <cristian.agu14@gmail.com>
2018-02-05 10:21:58 -0500

    pruebas hasta conbenio de recaudos full

--- MercuryTours/src/test/convenios/ConveniosRecaudos.java
+++ MercuryTours/src/test/convenios/ConveniosRecaudos.java
@@ -1,13 +1,20 @@
 package test.convenios;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import base.TestBase;
 import objectsEdeq.Convenio;
 import objectsEdeq.CuentasBancarias;
+import objectsEdeq.LoginEdeq;
+import test.autebtificacion.edeq.LoginEdeqCorrecto;
 import utils.ExcelUtils;
 import utils.ExcelUtils.ExcelType;
 
 public class ConveniosRecaudos extends TestBase {
-	
+
+	LoginEdeqCorrecto loginEdeqCorrecto = new LoginEdeqCorrecto();
+
 	private Convenio convenio;
 
 	private String numeroConvenio = "";
@@ -31,6 +38,8 @@ public class ConveniosRecaudos extends TestBase {
 	private String posInicioFecha = "";
 
 	private String posFinfecha = "";
+	
+	private String cliente="";
 
 	/**
 	 * Codigo de la empresa que esta en la primera seccion del codigo de barras
@@ -46,30 +55,43 @@ public class ConveniosRecaudos extends TestBase {
 	private String usuario;
 
 	private String contrasena;
-	
+
 	static ExcelUtils excelDatosConvenio;
-	
+
+	@Test
 	public void ingresarConvenio() {
-		int indice = 1;
+		int indice = 3;
+		loginEdeqCorrecto.loginCorrecto();
 		try {
 			convenio = new Convenio(driver, "");
-			excelDatosConvenio = new ExcelUtils("convenio.xlsx", ExcelType.XLSX);
-			
-			numeroConvenio =  excelDatosConvenio.getCellData(1, indice);
+			excelDatosConvenio = new ExcelUtils("convenios.xlsx", ExcelType.XLSX);
+
+			numeroConvenio = excelDatosConvenio.getCellData(1, indice);
 			nombreconvenio = excelDatosConvenio.getCellData(2, indice);
-			posInicialCodEmpresa = excelDatosConvenio.getCellData(3, indice);
-			posFinCodEmpresa = excelDatosConvenio.getCellData(4, indice);
-			posInicialReferencia = excelDatosConvenio.getCellData(5, indice);
-			posFinalReferencia = excelDatosConvenio.getCellData(6, indice);
-			posInicialValor =excelDatosConvenio.getCellData(7, indice);
-			posFinValor = excelDatosConvenio.getCellData(8, indice);
-			posInicioFecha = excelDatosConvenio.getCellData(9, indice);
-			posFinfecha = excelDatosConvenio.getCellData(10, indice);
-			
+			posInicialCodEmpresa = excelDatosConvenio.getCellData(4, indice);
+			posFinCodEmpresa = excelDatosConvenio.getCellData(5, indice);
+			posInicialReferencia = excelDatosConvenio.getCellData(6, indice);
+			posFinalReferencia = excelDatosConvenio.getCellData(7, indice);
+			posInicialValor = excelDatosConvenio.getCellData(8, indice);
+			posFinValor = excelDatosConvenio.getCellData(9, indice);
+			posInicioFecha = excelDatosConvenio.getCellData(10, indice);
+			posFinfecha = excelDatosConvenio.getCellData(11, indice);
+			codigoEmpresa = excelDatosConvenio.getCellData(12, indice);
+			ip = excelDatosConvenio.getCellData(13, indice);
+			puerto = excelDatosConvenio.getCellData(14, indice);
+			ruta = excelDatosConvenio.getCellData(15, indice);
+			usuario = excelDatosConvenio.getCellData(16, indice);
+			contrasena = excelDatosConvenio.getCellData(17, indice);
+			cliente = excelDatosConvenio.getCellData(18, indice);
+
+			Assert.assertTrue(convenio.crearConvenioRecaudo(numeroConvenio, nombreconvenio, posInicialCodEmpresa,
+					posFinCodEmpresa, posInicialReferencia, posFinalReferencia, posInicialValor, posFinValor,
+					posInicioFecha, posFinfecha, ip, puerto, ruta, usuario, contrasena,cliente, codigoEmpresa));
+
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
+
 	}
 
 }
